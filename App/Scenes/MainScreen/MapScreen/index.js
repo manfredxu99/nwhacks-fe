fd15fedcb2ddbb95f5be9df8ef0d546668a49b22
Manfred Xu <manfredxu99@gmail.com>
2021-01-10 09:46:16 -0800

    Map working

--- App/Scenes/MainScreen/MapScreen/index.js
+++ App/Scenes/MainScreen/MapScreen/index.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import { View } from 'react-native'
+import StyleSheet from 'App/Themes/StyleSheet'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+
+const styles = StyleSheet.create({})
+
+const API_KEY = 'AIzaSyABlr-bkdikJ3ErSH7ibWD1nY0xePUaA4Y'
 
 const MapScreen = () => {
-  return <View></View>
+  return (
+    <View style={styles.flex1}>
+      <MapView
+        style={styles.flex1}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 49.238866,
+          longitude: -123.050542,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  )
 }
 
 export default MapScreen
