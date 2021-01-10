74feb134f8e1b1b06f5f81ba524c92c8cd005481
Manfred Xu <manfredxu99@gmail.com>
2021-01-10 11:01:53 -0800

    Callouts

--- App/Scenes/MainScreen/MapScreen/MapMarker.js
+++ App/Scenes/MainScreen/MapScreen/MapMarker.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Marker } from 'react-native-maps'
+import { Callout, Marker } from 'react-native-maps'
 import { View } from 'react-native'
 import Text from 'App/Components/Text'
 
@@ -16,8 +16,33 @@ function getPinColor(riskLevel) {
   return 'green'
 }
 
-const MapMarker = ({ riskLevel, ...props }) => {
-  return <Marker {...props} pinColor={getPinColor(riskLevel)}></Marker>
+function getText(riskLevel) {
+  if (riskLevel >= 10) {
+    return 'Exercise high caution'
+  }
+  if (riskLevel >= 5) {
+    return 'Exercise moderate to high caution'
+  }
+  if (riskLevel >= 2) {
+    return 'Exercise moderate caution'
+  }
+  return 'This area is safe'
+}
+
+const MapMarker = ({ riskLevel, confirmed, title, ...props }) => {
+  const color = getPinColor(riskLevel)
+  return (
+    <Marker title="dickhead" {...props} pinColor={color}>
+      <Callout>
+        <View style={{ borderRadius: 10 }}>
+          <Text>{title}</Text>
+          <Text>Confirmed: {confirmed}</Text>
+          <Text>In contact: {riskLevel}</Text>
+          <Text style={{ color }}>{getText(riskLevel)}</Text>
+        </View>
+      </Callout>
+    </Marker>
+  )
 }
 
 export default MapMarker
--- App/Scenes/MainScreen/MapScreen/index.js
+++ App/Scenes/MainScreen/MapScreen/index.js
@@ -14,42 +14,49 @@ const MOCK_MARKERS = [
     latitude: 49.20407073449304,
     longitude: -123.13492625258617,
     risk: 15,
+    confirmed: 5,
   },
   {
     title: 'White Spot',
     latitude: 49.20477278183299,
     longitude: -123.1308799827935,
     risk: 12,
+    confirmed: 7,
   },
   {
     title: `Cafe de l'Orangerie`,
     latitude: 49.20421333841166,
     longitude: -123.13502449032984,
     risk: 8,
+    confirmed: 3,
   },
   {
     title: `McDonald's`,
     latitude: 49.20610357755158,
     longitude: -123.12544492516429,
     risk: 6,
+    confirmed: 5,
   },
   {
     title: `St Augustine's Church`,
     latitude: 49.207239116082015,
     longitude: -123.13427475715626,
     risk: 5,
+    confirmed: 5,
   },
   {
     title: `Chevron`,
     latitude: 49.210470351384565,
     longitude: -123.12972573083604,
     risk: 3,
+    confirmed: 2,
   },
   {
     title: `Amway Distributor`,
     latitude: 49.20636158239532,
     longitude: -123.13194859762149,
     risk: 1,
+    confirmed: 1,
   },
 ]
 
@@ -72,7 +79,9 @@ const MapScreen = () => {
               latitude: marker.latitude,
               longitude: marker.longitude,
             }}
+            title={marker.title}
             riskLevel={marker.risk}
+            confirmed={marker.confirmed}
           />
         ))}
       </MapView>
